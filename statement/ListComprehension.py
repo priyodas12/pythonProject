0fe4a0e01297d625a897e493ff8c0b260675ae74
Priyobrato Das <priyodas12@gmail.com>
2021-01-24 10:00:39 +0530

    1

---
+++ statement/ListComprehension.py
@@ -0,0 +1,13 @@
+myString1="hello"
+mylist1=[]
+for letter in myString1:
+    mylist1.append(letter)
+print(mylist1)
+
+myString2="helloWorld"
+mylist2=[letter for letter in myString2]
+print(mylist2)
+
+# first num is for appending that number ,2nd is for iteration
+mylist3=[num**2 for num in range(0,10)]
+print(mylist3)

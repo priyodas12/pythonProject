3b6ba9b463e89a48da519628082e8ff1483ef887
Priyobrato Das <priyodas12@gmail.com>
2021-01-25 09:21:33 +0530

    6

---
+++ functions/FunctionInteraction.py
@@ -0,0 +1,27 @@
+from random import shuffle
+
+
+def shuffle_list(mylist):
+    shuffle(mylist)
+    return mylist
+
+
+def player_guess():
+    guess = ''
+    while guess not in ['0', '1', '2']:
+        guess = input("pick a number 0,1 or 2:")
+    return int(guess)
+
+
+def check_guess(mylist, guess):
+    if mylist[guess] == 'O':
+        print('correct')
+    else:
+        print('wrong!')
+        print(mylist)
+
+
+my_list = ['', 'O', '']
+mixed_list = shuffle_list(my_list)
+guess = player_guess()
+check_guess(mixed_list, guess)

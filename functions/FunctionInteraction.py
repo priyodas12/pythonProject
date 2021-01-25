5a10e5c62e4f8acc180231b5ffde881d75db169b
Priyobrato Das <priyodas12@gmail.com>
2021-01-25 09:28:57 +0530

    7

--- functions/FunctionInteraction.py
+++ functions/FunctionInteraction.py
@@ -9,7 +9,7 @@ def shuffle_list(mylist):
 def player_guess():
     guess = ''
     while guess not in ['0', '1', '2']:
-        guess = input("pick a number 0,1 or 2:")
+        guess = input("pick a number 0,1 or 2::")
     return int(guess)
 
 

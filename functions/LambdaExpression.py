5ef728a63e0aea90e31eeb5ea9b3c421fca8ab8b
Priyobrato Das <priyodas12@gmail.com>
2021-01-25 12:26:14 +0530

    11

--- functions/LambdaExpression.py
+++ functions/LambdaExpression.py
@@ -1,4 +1,19 @@
+my_nums = [1, 2, 3, 4, 5]
+
+
 def square(num):
-    return num**2
-my_nums=[1,2,3,4,5]
-print(list(map(square,my_nums)))
+    return num ** 2
+
+
+print(list(map(square, my_nums)))
+
+
+def check_even(num):
+    return num % 2 == 0
+
+
+print(list(filter(check_even, my_nums)))
+
+print(list(map(lambda num:num**2,my_nums)))
+
+print(list(filter(lambda num:num%2==0,my_nums)))

a102fe08b363116afb4b76c0034aa93fd439c8d1
Priyobrato Das <priyodas12@gmail.com>
2021-01-25 08:59:01 +0530

    5

--- functions/FunctionLogic.py
+++ functions/FunctionLogic.py
@@ -1,15 +1,18 @@
-work_hours=[('abc',486),('bca',233),('cab',645),('dws',432)]
+work_hours = [('abc', 486), ('bca', 233), ('cab', 645), ('dws', 432)]
+
 
 def find_best_emp(workhours):
-    current_max=0
-    current_best_emp=''
-    for emp,hr in workhours:
-        if hr>current_max:
-            current_max=hr
-            current_best_emp=emp
+    current_max = 0
+    current_best_emp = ''
+    for emp, hr in workhours:
+        if hr > current_max:
+            current_max = hr
+            current_best_emp = emp
         else:
             pass
 
-    return (current_best_emp,current_max)
+    return current_best_emp, current_max
 
-print(find_best_emp(work_hours))
+
+name, hours = find_best_emp(work_hours)
+print(f'{name} worked {hours}')

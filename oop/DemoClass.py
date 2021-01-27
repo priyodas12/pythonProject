348930e1e80adf83938f032c83c40183d6ba80fa
Priyobrato Das <priyodas12@gmail.com>
2021-01-27 09:54:32 +0530

    14

--- oop/DemoClass.py
+++ oop/DemoClass.py
@@ -1,8 +1,16 @@
 class Demo():
-    def __init__(self,type,valid):
-        self.type=type
-        self.valid=valid
+    # CLASS object attributes:
+    amount = 100
+    fixed_rate = 182.56
+
+    # constructor of class
+    def __init__(self, type, valid):
+        # attribute we take in the argument
+        # actual attribute assignment done here:
+        self.mytype = type
+        self.validity = valid
 
 
-demo=Demo("sample",True)
-print(f"{demo} {demo.type} {demo.valid}")
+demo = Demo(type="special",valid=True)
+
+print(f"{demo} {demo.mytype} {demo.validity} {demo.amount} {demo.fixed_rate}")
